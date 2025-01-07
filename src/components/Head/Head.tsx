02e8c62d3833a5dbdf9f1cc0cab66eab38bf933e
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-01-07 12:07:48 +0200

    fix for safari

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,21 +1,26 @@
 'use client';
 
 import { useTheme } from '@mui/material';
+import { useEffect } from 'react';
 
 function Head() {
   const theme = useTheme();
 
+  useEffect(() => {
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
+    if (metaThemeColor) {
+      metaThemeColor.setAttribute('content', theme.palette.background.default);
+    }
+  }, [theme.palette.background.default]);
+
   return (
     <>
-      <meta name="theme-color" content={theme.palette.background.default} />
-      <meta
-        name="apple-mobile-web-app-status-bar-style"
-        content={theme.palette.background.default}
-      />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
       />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
     </>
   );
 }
