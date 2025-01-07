feb9861051e49c34fdffb878e94e7913e28275de
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-01-07 12:17:31 +0200

    fix for safari

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -11,12 +11,18 @@ function Head() {
 
     if (metaThemeColor) {
       metaThemeColor.setAttribute('content', theme.palette.background.default);
+
+      const newMetaThemeColor = document.createElement('meta');
+      newMetaThemeColor.name = 'theme-color';
+      newMetaThemeColor.content = theme.palette.background.default;
+
+      document.head.removeChild(metaThemeColor);
+      document.head.appendChild(newMetaThemeColor);
     }
   }, [theme.palette.background.default]);
 
   return (
     <>
-      <meta name="theme-color" content={theme.palette.background.default} />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
