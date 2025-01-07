e5c6476f6a4827b2472449104e42755553a15076
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-01-07 12:25:48 +0200

    fix for safari

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,13 +1,7 @@
-'use client';
-
-import { useTheme } from '@mui/material';
-
 function Head() {
-  const theme = useTheme();
-
   return (
     <>
-      <meta name="theme-color" content={theme.palette.accentPink.main} />
+      <meta name="theme-color" content="#ff2d55" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta
         name="viewport"
